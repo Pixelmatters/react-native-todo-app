28b61f5b3498ee3922653e7e5cf6eae24579265e
Tiago Pereira <tiagopereira@wearepixelmatters.com>
2020-07-21 12:13:28 +0100

    feat: inital commit

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {SafeAreaView} from 'react-native';
+
+export default function App() {
+  return <SafeAreaView></SafeAreaView>;
+}
