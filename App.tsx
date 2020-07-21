ab23b366498c738ad8b604adfc6d8c1699852db3
Tiago Pereira <tiagopereira@wearepixelmatters.com>
2020-07-21 12:19:23 +0100

    feat(app): updated app.tsx and wrapped the application with the store

--- App.tsx
+++ App.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import 'react-native-gesture-handler';
-import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {HomeNavigator} from './src/navigators/HomeNavigator';
+import {Provider} from 'react-redux';
+import {getInitialState, todoReducer} from './src/store/Todo/reducers';
+import {ITodoState} from './src/store/Todo/types';
+import {Text} from 'react-native';
+import {createStore} from 'redux';
 
 export default function App() {
-  return <SafeAreaView></SafeAreaView>;
+  const [currentState, setCurrentState] = useState<ITodoState>();
+
+  useEffect(() => {
+    async function handleInitialState() {
+      const initialState = await getInitialState();
+      setCurrentState(initialState);
+    }
+    handleInitialState();
+  }, []);
+
+  if (!currentState) {
+    return <Text>Loading ...</Text>;
+  }
+  const store = createStore(todoReducer, currentState);
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <HomeNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
 }
